eeed527c54954965c6ffaac3fe82a51887359c44
Choadis <dahlgrensm@gmail.com>
2018-11-05 03:58:47 -0500

    Barebones front end

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 var express = require('express');        // call express
 var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
-var reload = require('reload')
+var path = require('path');
 
 // configure app to use bodyParser()
 // this will get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 
 var Hero     = require('./models/hero.js');
 var Villain     = require('./models/villain.js');
 var Environment     = require('./models/environment.js');
 var mongoose   = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/sotm_db"); // connect to the database
+mongoose.connect("mongodb://localhost:27017/sotm_db", { useNewUrlParser: true }); // connect to the database
 
 var router = express.Router();              // get an instance of the express Router
 
@@ -108,11 +108,14 @@ router.get('/environment', (req, res) => {
   });
 });
 
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname+'/views/index.html'));
+});
+
 app.use('/api', router);
 
 // START THE SERVER
 // =============================================================================
-reload(app);
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
