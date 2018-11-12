120ead751bb4461f406c9ff576f6817ff350c4c9
Choadis <dahlgrensm@gmail.com>
2018-11-12 00:20:56 -0500

    Validates email input through regex

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ var UserSchema = new mongoose.Schema({
     type: String,
     unique: true,
     required: true,
+    match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     trim: true
   },
   username: {
