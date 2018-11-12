f5a22c51c2ca48bcfcba56918ec8f70699f04142
Choadis <dahlgrensm@gmail.com>
2018-11-12 00:59:32 -0500

    Auth works

--- server.js
+++ server.js
@@ -183,9 +183,44 @@ router.delete('/user/:userID', (req, res, next) => {
   User.deleteOne({ _id: req.params.userID })
   .exec()
   .then(result => {
-    res.status(200).json({ 
+    res.status(200).json({
       message: "User deleted"
     });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    });
+  });
+
+});
+
+router.post('/login', (req, res, next) => {
+
+  User.findOne( {email: req.body.email} )
+  .exec()
+  .then(user => {
+    if (user.length < 1){
+      return res.status(401).json({
+        message: "Authentication failed"
+      })
+    }
+    bcrypt.compare(req.body.password, user.password, (err, result) => {
+      if (err) {
+        return res.status(401).json({
+          message: "Auth failed"
+        })
+      }
+      if (result) {
+        return res.status(200).json({
+          message: "Sucessful auth"
+        })
+      }
+      return res.status(401).json({
+        message: "Auth failed"
+      })
+    })
   })
   .catch(err => {
     console.log(err);
